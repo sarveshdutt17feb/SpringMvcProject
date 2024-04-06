41e10540e9423e2c5d1876dec0bdbb23160c5627
Sarvesh Dutt Richhariya <sarveshdutt40@gmail.com>
2024-04-06 23:53:35 +0530

    http method support in detail "@RequestMapping(value="",method=Request.GET)

--- src/main/java/com/lcwd/mc/SpringMvcProject/controllers/ApiController.java
+++ src/main/java/com/lcwd/mc/SpringMvcProject/controllers/ApiController.java
@@ -2,6 +2,7 @@ package com.lcwd.mc.SpringMvcProject.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,15 +12,20 @@ import java.util.List;
 @RestController
 @RequestMapping("/api")
 public class ApiController {
-    @RequestMapping("/hello")
-
+    @RequestMapping(value = "/hello",method = RequestMethod.GET)
     public String helloApi(){
         return "Hello , how are you";
     }
-    @RequestMapping("/users")
+    @RequestMapping(value = "/user",method = RequestMethod.GET)
 
     public List<String> getUserData(){
         return Arrays.asList("Ram","shyam","chikku");
 
+    }
+
+    @RequestMapping(value = "/create-user",method = RequestMethod.POST)
+    public String createuser(){
+        System.out.println("creating user");
+        return "creating user";
     }
 }
